614c135d93866e02b7c77aec51f96188400bb677
Mike Dodson <thinkwellpub@gmail.com>
2021-10-16 20:31:52 -0500

    node-complete course
    1st express app. Using Express router.

--- app.js
+++ app.js
@@ -3,21 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-// Use to parse instead of body-parser
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send("<form action='/product' method='POST'><input type='text' name='title' placeholder='title'></input><button type='submit'>Add Product</button></form>");
-});
-
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello world from Express!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("You are now live on port 3000!");
