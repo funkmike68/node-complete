308978fd88ff45ab4740fa7569ff5f701f41e796
Mike Dodson <thinkwellpub@gmail.com>
2021-10-16 19:50:43 -0500

    node-complete course
    1st express app. Parsing incoming requests.

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+// Use to parse instead of body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/add-product", (req, res, next) => {
-  console.log("Using express middleware");
-  res.send("<h1>The Add Product Page</h1>");
+  res.send("<form action='/product' method='POST'><input type='text' name='title' placeholder='title'></input><button type='submit'>Add Product</button></form>");
+});
+
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("Using express middleware");
   res.send("<h1>Hello world from Express!</h1>");
 });
 
