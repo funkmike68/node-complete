68c1c41cbf4d227b40369fc7c16ae7be19e85a26
Mike Dodson <thinkwellpub@gmail.com>
2021-10-19 09:49:28 -0500

    node-complete course
    1st express app. Converted html pages to ejs.

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//app.set("views", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(process.env.PORT || 3000, () => {
